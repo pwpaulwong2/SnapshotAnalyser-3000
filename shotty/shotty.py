1cf1b3ec0e720ef67ab484a832a68ba72c899119
pwpaulwong2 <pwpaulwong2@gmail.com>
2020-06-23 15:18:05 +1000

    Hanlde error from AWS when stopping a pending instance or starting a stopping one

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -81,8 +82,6 @@ def create_snapshot(project):
         print("Stopping {0}".format(i.id))
 
         i.stop()
-        i.wait_until_stopped()
-
         for v in i.volumes.all():
             print("   Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAlyser 3000")
@@ -123,7 +122,12 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0} ...".format(i.id))
-        i.stop()
+
+        try:
+           i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop {0}. ".format(i.id) + str(e))
+            continue
     return
 
 @instances.command('start')
@@ -135,7 +139,12 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0} ...".format(i.id))
-        i.start()
+        try:
+           i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}. ".format(i.id) + str(e))
+            continue
+
     return
 
 
